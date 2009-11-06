74183b7d543362de291295e825628daeba45410e
andre.normann <andre.normann@175ce79a-caaa-11de-8861-c343c794f7e0>
2009-11-06 13:39:17 +0000

    Added Point class

---
+++ src/Point.cpp
@@ -0,0 +1,22 @@
+/*
+ *  Point.cpp
+ *  zILDA
+ *
+ *  Created by Andre Normann on 06.11.09.
+ *  Copyright 2009 Andre Normann. All rights reserved.
+ *
+ */
+
+#include "Point.h"
+
+Point::Point(const QPointF& p, float z, const QColor& color, bool blanked)
+: _point(p)
+, _z(z)
+, _color(color)
+, _blanked(blanked)
+{
+}
+
+Point::~Point()
+{
+}	
